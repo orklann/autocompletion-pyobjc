70bcf12c78ef2e07369ad9a026abd2cf8096eb54
Aaron Elkins <threcius@yahoo.com>
2018-07-19 15:31:59 +0800

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+import inspect
+import Cocoa
+
+lookup = dict()
+
+def listModule(module):
+    for i in dir(module):
+        if not lookup.get(i):
+            print(i)
+            lookup.setdefault(i, True)
+        t = getattr(module, i)
+        if inspect.isclass(t):
+            listClass(t)
+
+def listClass(c):
+    for i in dir(c):
+        if not lookup.get(i):
+            print(i)
+            lookup.setdefault(i, True)
+
+listModule(Cocoa)
